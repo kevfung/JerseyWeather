cd5d66dc9a4fe03b0d3b845717da56a522ccbf77
kevfung <kevfung@users.noreply.github.com>
2017-03-03 22:21:49 -0800

    Read Open Weather API key from gradle.properties file when the application class is loaded

--- src/main/java/com/kevfung/WeatherServiceApplication.java
+++ src/main/java/com/kevfung/WeatherServiceApplication.java
@@ -1,7 +1,14 @@
 package com.kevfung;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 import javax.ws.rs.ApplicationPath;
 
+import org.eclipse.jetty.util.log.Slf4jLog;
 import org.glassfish.jersey.server.ResourceConfig;
 
 /**
@@ -11,10 +18,43 @@ import org.glassfish.jersey.server.ResourceConfig;
  *
  */
 @ApplicationPath("")
-public class WeatherServiceApplication extends ResourceConfig {    
+public class WeatherServiceApplication extends ResourceConfig {
+	
+	private static final Slf4jLog LOG = new Slf4jLog(WeatherServiceApplication.class.getName());
+	private static final String PROPERTIES_FILE = "gradle.properties";
+	
+	public static String openWeatherApiKey;
 	
 	public WeatherServiceApplication() {
 		// Add our packages as resources for this application
 		packages("com.kevfung");
+		
+		loadOpenWeatherApiKey();
+	}
+	
+	/**
+	 * Get Open Weather API key from configuration properties file and load
+	 * it into the local static variable
+	 */
+	private void loadOpenWeatherApiKey() {
+		try (Stream<String> stream = Files.lines(Paths.get(PROPERTIES_FILE))) {
+
+			Optional<String> firstLine = stream.findFirst();
+			if (firstLine.isPresent()) {
+				final String keyValue = firstLine.get().toString();
+				final int indexOfEqual = keyValue.indexOf("=");
+				if (indexOfEqual >= 0) {
+					openWeatherApiKey = keyValue.substring(indexOfEqual+1);
+				} 
+				else {
+					LOG.warn("Could not retrieve Open Weather API key from " + PROPERTIES_FILE);
+				}
+			}
+			else {
+				LOG.warn("Could not retrieve Open Weather API key from " + PROPERTIES_FILE);
+			}	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
 	}
 }
