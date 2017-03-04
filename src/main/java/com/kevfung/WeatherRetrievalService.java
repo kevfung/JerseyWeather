a245175f101de0e085e9bff4479ce18f08c1b5ee
kevfung <kevfung@users.noreply.github.com>
2017-03-04 13:34:55 -0800

    Added functionality to communicate with Open Weather API and retrieve current weather for Vancouver

--- src/main/java/com/kevfung/WeatherRetrievalService.java
+++ src/main/java/com/kevfung/WeatherRetrievalService.java
@@ -4,10 +4,16 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+
+import org.apache.log4j.Logger;
+
+import com.kevfung.utils.OpenWatherApiUtils;
 
 /**
  * Service used to communicate with Open Weather APi to retrieve 
@@ -19,12 +25,23 @@ import javax.ws.rs.core.UriInfo;
 @Path("/openweather")
 public class WeatherRetrievalService {
 
+	private static final Logger LOG = Logger.getLogger(WeatherRetrievalService.class);
+	
 	@GET
 	@Path("/current")
 	public Response getCurrentWeather(@Context UriInfo queryParams) {
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("");
+		WebTarget target = client.target(OpenWatherApiUtils.BASE_URL)
+				.path(OpenWatherApiUtils.WEATHER_RESOURCE)
+				.queryParam(OpenWatherApiUtils.CITY_QUERY_PARAM, "Vancouver")
+				.queryParam(OpenWatherApiUtils.API_KEY_QUERY_PARAM, OpenWatherApiUtils.getOpenWeatherApiKey());
+
+		LOG.debug("Target URL: " + target.getUri().toString());
 		
-		return null;
+		Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+		
+		String jsonResponse = invocationBuilder.get(String.class);
+		
+		return Response.status(200).entity(jsonResponse).build();
 	}
 }
