ec7d6c328acaa5f01f0ab50d51b8adf6098f725e
kevfung <kevfung@users.noreply.github.com>
2017-03-03 22:24:46 -0800

    Renamed a parameter in our getTestMsg method

--- src/main/java/com/kevfung/WeatherService.java
+++ src/main/java/com/kevfung/WeatherService.java
@@ -25,21 +25,21 @@ public class WeatherService {
 	 * This method is for testing our weather service.
 	 * 
 	 * @param msg String from the given path
-	 * @param info Any query parameters passed in
+	 * @param queryParams Any query parameters passed in
 	 * @return outputs a message containing the URI parameter and any query parameters if they exist
 	 */
 	@GET
 	@Path("/{param}")
-	public Response getTestMsg(@PathParam("param") String msg, @Context UriInfo info) {
+	public Response getTestMsg(@PathParam("param") String msg, @Context UriInfo queryParams) {
 		StringBuffer output = new StringBuffer("Your URI parameter: " + msg);	
 		
-		MultivaluedMap<String, String> queryParam = info.getQueryParameters();
+		MultivaluedMap<String, String> queryParam = queryParams.getQueryParameters();
 		Set<Entry<String, List<String>>> queryKeyValuePairs = queryParam.entrySet();
 		
 		for(Entry<String, List<String>> keyValuePair : queryKeyValuePairs) {
 			output.append("<br>").append(keyValuePair.getKey()).append(" - ").append(keyValuePair.getValue());
 		}		
-
+		
 		return Response.status(200).entity(output.toString()).build();
 	}
 }
