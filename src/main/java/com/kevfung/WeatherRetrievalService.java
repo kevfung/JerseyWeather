9d519e396567d604fcf1c59783ed4dbf95285d86
kevfung <kevfung@users.noreply.github.com>
2017-03-04 20:27:38 -0800

    Added object mapping of Open Weather JSON response to a POJO

--- src/main/java/com/kevfung/WeatherRetrievalService.java
+++ src/main/java/com/kevfung/WeatherRetrievalService.java
@@ -1,5 +1,8 @@
 package com.kevfung;
 
+import java.io.IOException;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.client.Client;
@@ -13,7 +16,9 @@ import javax.ws.rs.core.UriInfo;
 
 import org.apache.log4j.Logger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kevfung.utils.OpenWatherApiUtils;
+import com.kevfung.utils.jsonclasses.CurrentWeather;
 
 /**
  * Service used to communicate with Open Weather APi to retrieve 
@@ -29,19 +34,32 @@ public class WeatherRetrievalService {
 	
 	@GET
 	@Path("/current")
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response getCurrentWeather(@Context UriInfo queryParams) {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target(OpenWatherApiUtils.BASE_URL)
 				.path(OpenWatherApiUtils.WEATHER_RESOURCE)
 				.queryParam(OpenWatherApiUtils.CITY_QUERY_PARAM, "Vancouver")
+				.queryParam(OpenWatherApiUtils.UNITS_QUERY_PARAM, OpenWatherApiUtils.METRIC_UNITS)
 				.queryParam(OpenWatherApiUtils.API_KEY_QUERY_PARAM, OpenWatherApiUtils.getOpenWeatherApiKey());
 
 		LOG.debug("Target URL: " + target.getUri().toString());
 		
-		Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+		Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);		
+		String jsonResponse = invocationBuilder.get(String.class);		
+		LOG.debug(jsonResponse);
 		
-		String jsonResponse = invocationBuilder.get(String.class);
+		ObjectMapper objectMapper = new ObjectMapper();
+		CurrentWeather currentWeather = null;
+		try {
+			currentWeather = objectMapper.readValue(jsonResponse, CurrentWeather.class);
+			LOG.debug(currentWeather.toString());
+		} catch (IOException e) {
+			LOG.warn("Unable to parse JSON to " + CurrentWeather.class, e);
+		}		
 		
-		return Response.status(200).entity(jsonResponse).build();
+		return Response.status(200)
+				.entity(currentWeather != null ? currentWeather.toString().replace("\n", "<br>") : "Error: Unable to parse JSON response")
+				.build();
 	}
 }
