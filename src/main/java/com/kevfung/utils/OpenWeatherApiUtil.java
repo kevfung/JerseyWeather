28bb52e1053ffcebc6c6588b3c7c11b618011a2a
kevfung <kevfung@users.noreply.github.com>
2017-03-09 23:11:49 -0800

    Added comments to the various types of constants

--- src/main/java/com/kevfung/utils/OpenWeatherApiUtil.java
+++ src/main/java/com/kevfung/utils/OpenWeatherApiUtil.java
@@ -4,21 +4,29 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+
 import org.apache.log4j.Logger;
 
 public class OpenWeatherApiUtil {
 
 	private static final Logger LOG = Logger.getLogger(OpenWeatherApiUtil.class);
+	
+	// Properties file
 	private static final String PROPERTIES_FILE = "gradle.properties";
 	private static final String API_KEY_PROPERTY = "API_KEY";
 		
+	// URIs
 	public static final String BASE_URL = "http://api.openweathermap.org/data/2.5";
 	public static final String WEATHER_RESOURCE = "/weather";
 
+	// Query parameters & values
 	public static final String API_KEY_QUERY_PARAM = "APPID"; // Usually not safe to put API key in query param, but Open Weather API only allows this method
 	public static final String CITY_QUERY_PARAM = "q";
-	public static final String UNITS_QUERY_PARAM = "units";	
+	public static final String UNITS_QUERY_PARAM = "units";		
 	public static final String METRIC_UNITS = "metric";	// used by UNITS_QUERY_PARAM
+	
+	// Value constants
+	private static final String LOCALE_VANCOUVER = "Vancouver";
 	
 	private static String openWeatherApiKey; 
 	
